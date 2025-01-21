0cd5367c70dc6207684a8efdbda1cc5ccf7370c7
Juliana Ribeiro <juribeiro.ru@gmail.com>
2025-01-21 13:32:45 -0300

    feat: create service for app controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
 
   @Get() // empty means "/" and it will use home described on controller
   getHome(): string {
-    return "This is the home page";
+    return this.appService.getHome();
   }
 
   @Get("hello") // now URL should be http://localhost:3000/home/hello
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return "Hello World!";
   }
+
+  getHome(): string {
+    return "This is the home page";
+  }
 }
